56cc5905b0b655735ca6b59dfa67693e467154ee
jasonhnd <jasonhnd@users.noreply.github.com>
2026-06-28 23:13:57 +0900

    chore: keep Astro HTML compression enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
   output: 'static',
   outDir: './dist-astro',
   trailingSlash: 'never',
-  compressHTML: false,
+  compressHTML: true,
   build: {
     format: 'file',
   },
